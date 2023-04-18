ccf2b2e38197630dce551252e3b328e480453027
may <m4rch3n1ng@gmail.com>
2023-04-18 03:08:32 +0200

    add hash info to git prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,6 +2,28 @@ function __mayfish_git_path -d "get path of git directory"
 	echo (git rev-parse --git-dir 2> /dev/null)
 end
 
+function __mayfish_git_hash -d "format git hash"
+	set -l hash_long $argv[1]
+	set -l hash_short (git rev-parse --short $hash_long)
+	set -l hash_rel (git name-rev --no-undefined --always --exclude="tags/*" --exclude="remotes/*" "$hash_short" 2> /dev/null)
+
+	set -l spl (string split " " $hash_rel)
+	if test "$spl[1]" = "$spl[2]"
+		set hash_rel (git name-rev --no-undefined --always --exclude="tags/*" "$hash_short" 2> /dev/null)
+		set spl (string split " " $hash_rel)
+
+		if test "$spl[1]" = "$spl[2]"
+			set --erase hash_rel
+		end
+	end
+
+	if [ $hash_rel ]
+		echo $hash_rel
+	else
+		echo $hash_short
+	end
+end
+
 # thx https://github.com/ohmyzsh/ohmyzsh/blob/d47e1d65f66f9bb2e7a96ba58797b33f0e91a623/themes/peepcode.zsh-theme#L14
 # thx https://github.com/zthxxx/jovial/blob/bd705f1b74ecb1dfc5a3637193498191a016bb09/jovial.zsh-theme#L749
 function __mayfish_git_mode -d "get info for current git operation"
@@ -33,23 +55,20 @@ function __mayfish_git_mode -d "get info for current git operation"
 
 		echo "bsc$branch"
 	else if test -e "$git_path/MERGE_HEAD"
-		set -l merge_long (cat "$git_path/MERGE_HEAD")
-		set -l merge (git rev-parse --short $merge_long)
-		set -l mrg (git name-rev --no-undefined --always --exclude="tags/*" "$merge")
+		set -l hash_long (cat "$git_path/MERGE_HEAD")
+		set -l hash (__mayfish_git_hash $hash_long)
 
-		echo "mrg :$mrg"
+		echo "mrg :$hash"
 	else if test -f "$git_path/CHERRY_PICK_HEAD"
-		set -l pick_long (cat "$git_path/CHERRY_PICK_HEAD")
-		set -l pick (git rev-parse --short $pick_long)
-		set -l chp (git name-rev --no-undefined --always --exclude="tags/*" "$pick")
+		set -l hash_long (cat "$git_path/CHERRY_PICK_HEAD")
+		set -l hash (__mayfish_git_hash $hash_long)
 
-		echo "chp :$chp"
+		echo "chp :$hash"
 	else if test -f "$git_path/REVERT_HEAD"
-		set -l revert_long (cat "$git_path/REVERT_HEAD")
-		set -l revert (git rev-parse --short $revert_long)
-		set -l rvt (git name-rev --no-undefined --always --exclude="tags/*" "$revert")
+		set -l hash_long (cat "$git_path/REVERT_HEAD")
+		set -l hash (__mayfish_git_hash $hash_long)
 
-		echo "rvt :$rvt"
+		echo "rvt :$hash"
 	end 
 end
 
@@ -63,7 +82,8 @@ function __mayfish_git_branch -d "get name of current branch / hash"
 	else
 		[ $ret = 128 ] && return
 		set ref (git rev-parse --short HEAD 2> /dev/null) || return
-		echo ':'$ref
+		set -l hash (__mayfish_git_hash $ref)
+		echo ':'$hash
 	end
 end
 
@@ -100,7 +120,6 @@ function fish_prompt -d "generate prompt"
 	set -l usr $yellow(whoami)$normal
 	set -l dir $magenta(basename (prompt_pwd))$normal
 	set -l git (__mayfish_git)
-
 
 	if test $git
 		echo -ns $start' '$usr' '$dir' '$git$normal' >> ' 
