1e00ccef95c03b033d1ad49d2eebbcf8c4520c21
may <m4rch3n1ng@gmail.com>
2023-04-24 22:42:26 +0200

    add am & am/rbs mode, exclude bisect from name-rev

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -5,7 +5,7 @@ end
 function __mayfish_git_hash -d "format git hash"
 	set -l hash_long $argv[1]
 	set -l hash_short (git rev-parse --short $hash_long)
-	set -l hash_rel (git name-rev --no-undefined --always --exclude="tags/*" --exclude="remotes/*" "$hash_short" 2> /dev/null)
+	set -l hash_rel (git name-rev --no-undefined --always --exclude="tags/*" --exclude="remotes/*" --exclude="bisect/*" "$hash_short" 2> /dev/null)
 
 	set -l spl (string split " " $hash_rel)
 	if test "$spl[1]" = "$spl[2]"
@@ -24,15 +24,27 @@ function __mayfish_git_hash -d "format git hash"
 	end
 end
 
-# thx https://github.com/ohmyzsh/ohmyzsh/blob/d47e1d65f66f9bb2e7a96ba58797b33f0e91a623/themes/peepcode.zsh-theme#L14
-# thx https://github.com/zthxxx/jovial/blob/bd705f1b74ecb1dfc5a3637193498191a016bb09/jovial.zsh-theme#L749
+# thx https://github.com/Byron/gitoxide/blob/31801420e1bef1ebf32e14caf73ba29ddbc36443/gix/src/repository/state.rs#L3
+# thx https://github.com/Byron/gitoxide/blob/31801420e1bef1ebf32e14caf73ba29ddbc36443/gix/src/state.rs#L3
 function __mayfish_git_mode -d "get info for current git operation"
 	set -l git_path $(__mayfish_git_path)
-	set -l green (set_color green)
 
-	if test -e "$git_path/rebase"; or test -e "$git_path/rebase-apply"; or test -e "$git_path/rebase-merge"
+	if test -f "$git_path/rebase-apply/applying"
+		echo "am"
+	else if test -f "$git_path/rebase-apply/rebasing"
+		# todo rebase steps / extra info ?
+		# idk how to get into this mode lol
+		echo "rbs"
+	else if test -d "$git_path/rebase-apply"
+		echo "am/rbs"
+	else if test -d "$git_path/rebase-merge"
 		set -l branch
 		set -l proc
+
+		if test -f "$git_path/rebase-merge/head-name"
+			set -l branch_str (cat "$git_path/rebase-merge/head-name" | sed -e 's/^refs\/heads\///')
+			[ $branch_str ] && set branch " $branch_str"
+		end
 
 		if test -f "$git_path/rebase-merge/msgnum"
 			set -l step (cat "$git_path/rebase-merge/msgnum")
@@ -40,13 +52,8 @@ function __mayfish_git_mode -d "get info for current git operation"
 			set proc " $step/$total"
 		end
 
-		if test -f "$git_path/rebase-merge/head-name"
-			set -l branch_str (cat "$git_path/rebase-merge/head-name" | sed -e 's/^refs\/heads\///')
-			[ $branch_str ] && set branch " $green$branch_str"
-		end
-
-		echo "rbs$proc$branch"
-	else if test -e "$git_path/BISECT_LOG"
+		echo "rbs$branch$proc"
+	else if test -f "$git_path/BISECT_LOG"
 		set -l branch
 		if test -f "$git_path/BISECT_START"
 			set -l branch_str (cat "$git_path/BISECT_START")
@@ -54,7 +61,7 @@ function __mayfish_git_mode -d "get info for current git operation"
 		end
 
 		echo "bsc$branch"
-	else if test -e "$git_path/MERGE_HEAD"
+	else if test -f "$git_path/MERGE_HEAD"
 		set -l hash_long (cat "$git_path/MERGE_HEAD")
 		set -l hash (__mayfish_git_hash $hash_long)
 
